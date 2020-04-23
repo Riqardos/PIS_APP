15cae5e6cf39d7952f4cc08019e5c54bb2e6415f
Richard Kaštiak <kastiak.richard@gmail.com>
2020-04-23 19:33:25 +0200

    added entity

--- CREATE_RUN/views.py
+++ CREATE_RUN/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from zeep import Client
-
+import random
 
 # Create your views here.
 def view_home(request):
@@ -8,10 +8,36 @@ def view_home(request):
 
 
 def view_page(request):
+    print("")
+
     return render(request, 'CREATE_RUN/page.html', {})
 
 
 def view_stanovisko(request):
+    if request.POST:
+        print(request.POST)
+
+
+        beh = {
+            "id": random.randint(1, 10),
+            "name": request.POST['name'],
+            "stafeta": True if request.POST.get('typ') == '0' else False,
+            "datum_konania": request.POST['termin'],
+            "kapacita": int(request.POST.get('kapacita')) if request.POST.get('kapacita') else 0,
+            "podporny_tim": True if request.POST.get('podporny_tim') else False,
+            "prihlaseni": 0,
+            "zakladna_cena_behu": 0
+        }
+
+        stanovisko_names = request.POST.getlist('stanovisko_name')
+        obcerstvenia = request.POST.getlist('obcerstvenie')
+
+        print(beh)
+
+
+
+
+
     client = Client('http://pis.predmety.fiit.stuba.sk/pis/ws/GeoServices/Cities?WSDL')
     result_cities = client.service.getAll()
 
